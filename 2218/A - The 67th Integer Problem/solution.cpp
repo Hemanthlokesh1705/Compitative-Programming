bebd8c51fea6623ee45028c5209a1bc974b0c3fc
Hemanth L <hemanthlokesh58@gmail.com>
2026-06-30 23:15:47 +0530

    Add The 67th Integer Problem [A] from Codeforces

---
+++ 2218/A - The 67th Integer Problem/solution.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n;
+    cin>>n;
+    while(n){
+        int x;
+        cin>>x;
+        cout<<(x)<<endl;
+        n--;
+    }
+    return 0;
+}
